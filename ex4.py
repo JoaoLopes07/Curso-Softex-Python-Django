39de3977626128f36f5964c6b6fa047b5dc08a49
João Victor Lopes Dos Santos <133873524+JoaoLopes07@users.noreply.github.com>
2025-08-27 21:41:41 -0300

    Delete ex4.py

--- ex4.py
+++
@@ -1,7 +0,0 @@
-usuario = input('Digite o seu Usúario: ')
-senha = input('Digite sua senha: ')
-
-if 'admin' in usuario and len(senha) >=6:
-    print('Login Criado')
-else:
-    print('Usúario ou Senha inválido')
