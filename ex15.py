1143a1af7c2c3874e001504bad793fe890c2e9d3
João Victor Lopes Dos Santos <133873524+JoaoLopes07@users.noreply.github.com>
2025-08-27 21:40:20 -0300

    Delete ex15.py

--- ex15.py
+++
@@ -1,9 +0,0 @@
-palavra_proibida = 'spoiler'
-while True:
-    palavra = input('Digite a palavra até achar a proibida: ').lower()
-    if palavra_proibida in palavra:
-        print('Palavra proibida está na sua palavra')
-        break
-
-
-
