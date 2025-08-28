e9aa8c0281de243907b900463d4fe1da56e1efcc
João Victor Lopes Dos Santos <133873524+JoaoLopes07@users.noreply.github.com>
2025-08-27 21:39:07 -0300

    Delete ex11.py

--- ex11.py
+++
@@ -1,6 +0,0 @@
-frase = input('Digite uma frase: ')
-palavra = input('Digite uma palavra pra substituir: ')
-palavra2 = input('Qual palavra quer colocar no lugar? ')
-
-nova_frase = frase.replace(palavra, palavra2)
-print(nova_frase)
