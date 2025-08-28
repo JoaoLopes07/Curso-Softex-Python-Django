60871cb7c35c49ec8852713af5d16ce39ebe1586
João Victor Lopes Dos Santos <133873524+JoaoLopes07@users.noreply.github.com>
2025-08-27 21:41:09 -0300

    Delete ex2.py

--- ex2.py
+++
@@ -1,9 +0,0 @@
-senha = input('Digite uma senha com pelo menos 8 caracteres: ')
-
-caracteres = len(senha)
-
-if caracteres <8:
-    print('Digite pelo menos 8 caracteres')
-
-else:
-    print('Senha Válida')
