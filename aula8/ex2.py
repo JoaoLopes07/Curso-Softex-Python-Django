e4c9e54b6d5bb33c652209e22ae9007219b63197
julianasoftex <227845472+julianasoftex@users.noreply.github.com>
2025-09-01 12:11:39 -0300

    exclusao

--- aula8/ex2.py
+++
@@ -1,6 +0,0 @@
-palavra = input('Digite uma palavra: ').lower()
-
-nova_palavra = palavra.replace('a', '1').replace('e', '2').replace('i', '3').replace('o', '4').replace('u', '5')
-
-print(nova_palavra)
-print(palavra)
