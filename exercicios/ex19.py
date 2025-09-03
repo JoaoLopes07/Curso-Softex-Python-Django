c4b4851149d04250daff304e8f5825272383787d
julianasoftex <133873524+JoaoLopes07@users.noreply.github.com>
2025-09-03 10:23:33 -0300

    exercicios tupla

--- exercicios/ex19.py
+++ exercicios/ex19.py
@@ -1,9 +1,19 @@
+class Numero():
+    def __init__(self, numero):
+        self.numero = numero
+    
+    def maior(self):
+        for i in range(4):
+            maior = int(input('Digite outro número inteiro: '))
+
+        if maior > self.numero:
+            self.numero = maior
+        print(f'O maior número digitado foi o {self.numero}')
+
+
 numero = int(input('Digite um número inteiro: '))
 
-for i in range(4):
-    maior = int(input('Digite outro número inteiro: '))
+rodar = Numero(numero)
+rodar.maior()
 
-    if maior > numero:
-        numero = maior
 
-print(f'O maior número digitado foi o {numero}')
