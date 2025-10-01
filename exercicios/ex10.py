91e5101e4b32cb59c8b75b431e13730838a5668e
JoaoLopes07 <133873524+JoaoLopes07@users.noreply.github.com>
2025-10-01 11:32:55 -0300

    exclusao dos exercicios

--- exercicios/ex10.py
+++
@@ -1,8 +0,0 @@
-class Frase():
-    def __init__(self, frase):
-        self.frase = frase
-
-frase = input('Digite uma frase: ').title()
-print(frase)
-
-rodar = Frase(frase)
--- exercicios/ex9.py
+++
@@ -1,8 +0,0 @@
-class Texto():
-    def __init__(self, texto):
-        self.texto = texto
-
-texto = input('Digite um texto com espaços iniciais e finais: ').strip()
-print(texto)
-
-rodar = Texto(texto)
