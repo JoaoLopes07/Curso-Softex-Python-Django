56e9a3463dda49a58e930480298b6c3a596c071d
JoaoLopes07 <133873524+JoaoLopes07@users.noreply.github.com>
2025-11-10 11:38:44 -0300

    django_aula2

--- modulo04/core/admin.py
+++ modulo04/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Tarefa # 1. Importe seu Model
+from .models import Alunos
+# 2. "Registre" seu Model no sit
+admin.site.register(Tarefa)
+admin.site.register(Alunos)
--- modulo04/core/views.py
+++ modulo04/core/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tarefa
+from .models import Alunos
 
 def home(request):
+   todas_as_tarefas = Tarefa.objects.all()
+   todos_alunos = Alunos.objects.all()
    context = {
        'nome_usuario': 'João',
-       'tecnologias': {'Python', 'Django', 'HTML', 'CSS'}
+       'tecnologias': {'Python', 'Django', 'HTML', 'CSS'},
+       'tarefas': todas_as_tarefas,
+       'alunos': todos_alunos
    }
    #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
    return render(request, 'home.html', context)
