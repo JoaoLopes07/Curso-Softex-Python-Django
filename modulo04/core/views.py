d7cb078670dd010f47e120157f5ef14c572d3e72
JoaoLopes07 <133873524+JoaoLopes07@users.noreply.github.com>
2025-11-12 11:29:10 -0300

    update

--- modulo04/core/views.py
+++ modulo04/core/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Tarefa
 from .models import Alunos
 from .forms import TarefaForm
+
 def home(request):
     if request.method == 'POST':
         form = TarefaForm(request.POST)
@@ -11,19 +12,19 @@ def home(request):
             return redirect('home')
     else:
         form = TarefaForm() 
-   todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
-   todos_alunos = Alunos.objects.all()
-   context = {
-       'nome_usuario': 'João',
-       'tecnologias': {'Python', 'Django', 'HTML', 'CSS'},
-       'tarefas': todas_as_tarefas,
-       'form': form,
-       'alunos': todos_alunos
-   }
-   #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
-   return render(request, 'home.html', context)
+    
+
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
+    todos_alunos = Alunos.objects.all()
+    context = {
+        'nome_usuario': 'João',
+        'tecnologias': {'Python', 'Django', 'HTML', 'CSS'},
+        'tarefas': todas_as_tarefas,
+        'form': form,
+        'alunos': todos_alunos
+    }
+    return render(request, 'home.html', context)
 
 def login(request):
     return HttpResponse("<input>Login</input>")
-
 
