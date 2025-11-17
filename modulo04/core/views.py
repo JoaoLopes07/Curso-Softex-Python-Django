4ecbdf3a8a959ff2104c7bffe54248a0437d0bb6
Stefane-7 <133873524+JoaoLopes07@users.noreply.github.com>
2025-11-17 11:10:07 -0300

    CRUD

--- modulo04/core/views.py
+++ modulo04/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Tarefa
 from .models import Alunos
@@ -28,3 +28,14 @@ def home(request):
 def login(request):
     return HttpResponse("<input>Login</input>")
 
+def concluir_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        tarefa.concluida = True
+        tarefa.save()
+        return redirect('home')
+def deletar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        tarefa.delete()
+        return redirect('home')
