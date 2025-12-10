967c6457090d427f2c3c423c24595f423e4f5ddb
JoaoLopes07 <133873524+JoaoLopes07@users.noreply.github.com>
2025-12-10 11:35:58 -0300

    feat/adição das urls

--- modulo05/core/urls.py
+++ modulo05/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ListaTarefasAPIView, TarefasEstatisticasAPIView
+from .views import ListaTarefasAPIView, TarefasEstatisticasAPIView, DetalheTarefaAPIView, DuplicarTarefaAPIView
 
 app_name = 'core'
 urlpatterns = [
 
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
+    path('tarefas/<int:pk>/', DetalheTarefaAPIView.as_view(), name='detalhe-tarefa'),
     path('tarefas/estatisticas/', TarefasEstatisticasAPIView.as_view()),
+    path('tarefas/<int:pk>/duplicar/', DuplicarTarefaAPIView.as_view(), name='duplicar-tarefa'),
 ]
